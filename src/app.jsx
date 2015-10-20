82810369064fd3c5f78c65651f151f218e62ef19
Andreas Ronge <andreas.ronge@gmail.com>
2015-10-20 12:53:16 +0200

    some styling

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import api from './api';
 
-const NewsHeader = ({body}) => <div>Header {body}</div>;
+const NewsHeader = ({body}) => <p className="lead">Header {body}</p>;
 
-const NewsItem = (onSelect, {id, header}) => <div onClick={onSelect.bind(undefined, id)}>Item {id} - {header}</div>;
+const NewsItem = (onSelect, {id, header}) => <li><a href="#" onClick={onSelect.bind(undefined, id)}>Item {id} - {header}</a></li>;
 
 const NewsList = ({list, onSelect}) => <div>{list.map(NewsItem.bind(undefined, onSelect))}</div>;
 
@@ -25,7 +25,7 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="container">
         <NewsHeader body={this.state.body}/>
         <NewsList list={this.state.headers} onSelect={this.onSelect}/>
       </div>
